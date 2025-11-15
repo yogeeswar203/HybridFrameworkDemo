669e9443ac6a5f5e468027b2df64329a38b8a4e3
Yogeeswar Achari <yogeeswar203@gmail.com>
2025-11-15 22:22:43 +0530

    Hybrid framework commit 1

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -10,6 +10,6 @@ public class BasePage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+	//test
 
 }
---
+++ src/test/java/testBase/BaseClass3.java
@@ -0,0 +1,117 @@
+package testBase;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import org.apache.logging.log4j.LogManager;//log4j
+import org.apache.logging.log4j.Logger;   //log4j
+
+
+public class BaseClass3 {
+
+
+		static public WebDriver driver;
+		public Logger logger;
+		public Properties p;
+		
+		
+		@BeforeClass(groups= {"sanity","regression","master"})
+		@Parameters({"os", "browser"})
+		public void setup(String os, String br) throws IOException, InterruptedException
+		
+		{
+			//loading properties file
+			 FileReader file=new FileReader(".//src//test//resources//config.properties");
+			 p=new Properties();
+			 p.load(file);
+			
+			
+			//loading log4j 
+			logger=LogManager.getLogger(this.getClass());//Log4j
+					
+					
+			//launching browser based on condition
+			switch(br.toLowerCase())
+			{
+			case "chrome": driver=new ChromeDriver(); break;
+			case "edge": driver=new EdgeDriver(); break;
+			default: System.out.println("No matching browser..");
+						return;
+			}
+			
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+			
+			
+			driver.get(p.getProperty("appurlOrangeHRM")); // reading the URL from the properties file
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+			Thread.sleep(5000);
+		}
+		
+		@AfterClass(groups= {"sanity","regression","master"})
+		public void tearDown() throws InterruptedException
+		{
+			Thread.sleep(10000);
+			driver.quit();
+		}
+		
+
+		public String randomeString()
+		{
+			String generatedString=RandomStringUtils.randomAlphabetic(5);
+			return generatedString;
+		}
+		
+		public String randomeNumber()
+		{
+			String generatedString=RandomStringUtils.randomNumeric(10);
+			return generatedString;
+		}
+		
+		public String randomAlphaNumeric()
+		{
+			String str=RandomStringUtils.randomAlphabetic(3);
+			String num=RandomStringUtils.randomNumeric(3);
+			
+			return (str+"@"+num);
+		}
+		
+		public String captureScreen(String tname) throws IOException{
+			
+			String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+					
+			TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+			File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+			
+			String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".jpeg";
+			File targetFile=new File(targetFilePath);
+			
+			sourceFile.renameTo(targetFile);
+				
+			return targetFilePath;
+
+		}
+		
+		
+		
+	}
+
+
+
